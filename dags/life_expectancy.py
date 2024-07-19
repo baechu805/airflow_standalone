eadc78b38a2e1dc1d32182f120be8f219d8039f3
baechu805 <bjoo606@naver.com>
2024-07-19 16:26:44 +0900

    init

---
+++ dags/life_expectancy.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+with DAG(
+    'life_expectancy',
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        'depends_on_past': True,
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5)
+    },
+    description='life_expectancy DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2024, 7, 10),
+    catchup=True,
+    tags=['life_expectancy'],
+) as dag:
+
+    # t1, t2 and t3 are examples of tasks created by instantiating operators
+    t1 = BashOperator(
+        task_id='life_expectancy',
+        bash_command='life_expectancy',
+    )
+
+    
+
+
+    task_sex = DummyOperator(task_id='sex')
+    task_country = DummyOperator(task_id='country')
+    task_insurance = DummyOperator(task_id='insurance')
+    task_end = DummyOperator(task_id='end')
+    task_start = DummyOperator(task_id='start')
+
+    task_start >> t1
+    t1 >> task_country >>task_end
+    t1 >> task_sex >> task_end
+    t1 >> task_insurance >> task_end
+
